ac5712362df2ff0905eb18f01227a0245eec8981
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-08-30 21:20:32 -0300

    Adds Resume and Fix Email

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,8 @@ const about = {
   role: 'Full Stack Developer',
   description:
     'Brazilian Full Stack Developer working with React and NodeJS to bring websites to life.',
-  resume: 'https://example.com',
+  resume:
+    'https://www.linkedin.com/ambry/?x-li-ambry-ep=AQFInNFe5gKnVAAAAXuZiTY7VBQPKQo1KKaOhEh72LR_eGKKTSXBbmJhFYHnGtfhEFzm752YSJK_XiTeN4CHAiqqzPLeTUpUP7ovkoYXsGWZBgbwqvGneBcQOZlbjhSBvWSOjfp02nTj0wulCCQyvOGeb22ZWWh-HXd727azcLfcF2NHWD71dsbZuPTFMK1n5T_yrgPPC7AEHH1zHhNrLHZ5_pnhormM58iIZLlewXhY3Wow7YgSJhicLH22WCNHf_lCSuONjAmUypuYv6E-Gw__fjp5DxYy5nnJ8IccUBydS_j5-0GlvDdvf5jWOkjaHm1Ca97qwV0Az0pXm4p2WeujRC4_c_Pz6RG72dE72U2DWBSK3nONIIJB_B8lBsgVaxarOFba6yMsKXprb8Sq5xf5cGd_lhdhxba7UUvRtVW3MeKxBeKLTJsP5TvEKD9OfYgspQuQ_MflRwsDrgs6mrZcDKGwBu5Zf4yyHHbxUfJZes9sBA3ic-9WVOmffiFBGVXgJgMZ7QNmEQuNzl0lLw',
   social: {
     linkedin: 'https://www.linkedin.com/in/-vinicius-godoy/',
     github: 'https://github.com/vinicius-godoy',
@@ -59,7 +60,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'v.godoyrodriguesh@gmail.com',
+  email: 'v.godoyrodrigues@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
